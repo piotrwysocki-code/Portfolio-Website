8d99d8011ddd3d550c66d328b07946e9fee235eb
piotrwysocki-code <wysockpi@sheridancollege.ca>
2022-08-20 15:15:40 -0400

    Added form submit in js

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-$(()=>{
 
+class Connect {
+    constructor(name, email, subject, message){
+        this.name = name;
+        this.email = email;
+        this.subject = subject;
+        this.message = message;
+    }
+}
+
+$("#submitConnectForm").on('click', ()=>{   
+    submitConnectForm();
 })
 
-
+function submitConnectForm() {
+    let connectMsg = new Connect($("#name").val(), $("#email").val(), $("#subject").val(), $("#message").val());
+    
+    /*
+    $.ajax({
+        type : 'POST',
+        url : 'http://localhost:4000/send',
+        data: connectMsg,
+        dataType : 'json',
+        encode: true
+    }).done((results) => {
+            console.log(results);
+    });*/
+}
 
 
 
